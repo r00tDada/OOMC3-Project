c116a9dfb2c65bd47bc116bd1120f7fd78bad754
aadityagadhave <aadityagadhave@172.26.44.147>
2019-11-29 23:59:15 +0530

    ads

---
+++ src/gymmanagement/UserDetails.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gymmanagement;
+import javax.swing.*;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/**
+ *
+ * @author aadityagadhave
+ */
+
+
+public class UserDetails {
+
+    static ArrayList<UserDetails>user=new ArrayList<UserDetails>();
+    
+    private String name,last,us,gender,plan,email,password,address;
+    private int age,weight,height;
+
+    public UserDetails(String name, String last, String us, String gender, String plan, String email, String password, int age, int weight, int height,String address) {
+        this.name = name;
+        this.last = last;
+        this.us = us;
+        this.gender = gender;
+        this.plan = plan;
+        this.email = email;
+        this.password = password;
+        this.address = address;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    
+    public static void setUser(ArrayList<UserDetails> user) {
+        UserDetails.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public String getUs() {
+        return us;
+    }
+
+    public void setUs(String us) {
+        this.us = us;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPlan() {
+        return plan;
+    }
+
+    public void setPlan(String plan) {
+        this.plan = plan;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    
+    
+    
+    
+}
